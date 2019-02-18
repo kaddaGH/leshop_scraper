0cdf403ef6b2fab5b39864574384fac038394694
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-18 14:05:40 +0100

    Update scraper.

--- parsers/product_texts.rb
+++ parsers/product_texts.rb
@@ -1,8 +1,7 @@
 require './lib/headers'
 product = JSON.parse(content)
 
-description  = product[0]['foodFacts']['regulatedDescription'] rescue ''
-description = description.gsub(/[\n\s]/,' ').gsub(/,/,' ')
+description  = product[0]['foodFacts']['regulatedDescription'].gsub(/[\n\s,]/,' ') rescue ''
 product_details = page['vars']['product_details']
 product_details['PRODUCT_DESCRIPTION']= description
 product_details['_collection'] = 'products'
