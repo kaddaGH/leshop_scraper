19f9e23f7ea8a7163ff116faf58439459af7aa01
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-04-10 21:05:32 +0100

    Update header.

--- lib/headers.rb
+++ lib/headers.rb
@@ -12,6 +12,6 @@ module ReqHeaders
       "cookie" =>"__cfduid=df8c785565c236372fda70bc82c67745a1549792060; _ga=GA1.2.1232152974.1549792084; _fbp=fb.1.1549792085700.1570886238; _dy_c_exps=; _dycnst=dg; _dyid=7809242661650866999; _dy_c_att_exps=; _dyus_9876164=137%7C0%7C0%7C36%7C0%7C0.0.1552327481697.1552338012943.10531.0%7C69%7C11%7C2%7C119%7C6%7C0%7C0%7C0%7C0%7C0%7C0%7C6%7C0%7C0%7C0%7C0%7C0%7C6%7C0%7C0%7C0%7C0%7C0; _dycst=dk.m.c.ws.; _dy_geo=DZ.AF.DZ_.DZ__; _dy_df_geo=Algeria..; _dy_toffset=-2; _dy_ses_load_seq=41430%3A1552940220005; _dy_soct=1000907.1001003.1552940220; sidesStatus=%22%7B%5C%22currentLeft%5C%22%3Atrue%2C%5C%22currentRight%5C%22%3Atrue%2C%5C%22none%5C%22%3Afalse%2C%5C%22mono%5C%22%3Afalse%2C%5C%22twice%5C%22%3Atrue%2C%5C%22lastOpenedSide%5C%22%3A%5C%22left%5C%22%7D%22; _gid=GA1.2.1379008846.1554917644; _gcl_au=1.1.198820196.1554917644",
 
       "leshopch" => "eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6ImRjYjk1NjEyLTc2YmYtNDg4NC1hNWJkLWU3MDcyMmQ3ZjMxMSJ9.._lXDfzujC1Co3aQr.2KZwlwRyCCk5u0OaHF_Bo7jelO7JUCLdLl_RH7qVX1HqP8qu3a_8hFWvAwuvRvVJpRD1lXnXfQKLKhhqykyV4CqVZIkpm25lEOPinJuDcx4shAI_f85F3f8gIhJyHSmwq6kwwDaHvibZiX3vvNwEkdxvKyKiLtpvWTlV_pfI7JSBRdFl6FitLaQVhVgbcupHFWDV5MqG5qvVgvLVHrlh_U9Fy773kLytErZMOKIelvBUpXq6QsignP4OcBnGO034kYnyd-Uo1-XMcckcy_0dAVJQLZNN.FJrbm1i8fchHHOpF0G3mTg"
-
+  }
 end
 
