f3483b00f75f518382e4b16ab17d36f4cf5b96b9
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-04-10 20:36:45 +0100

    Update header.

--- lib/headers.rb
+++ lib/headers.rb
@@ -7,10 +7,11 @@ module ReqHeaders
       "Accept-Language" => "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
       "Accept-Encoding" => "gzip, deflate, br",
       "Referer" => "https://www.leshop.ch/de/search?query=Energy%20Drink",
-      "DNT" => "1",
+      "DNT" =>  "1",
       "Connection" => "keep-alive",
-      "cookie" => "__cfduid=df8c785565c236372fda70bc82c67745a1549792060; _ga=GA1.2.1232152974.1549792084; _fbp=fb.1.1549792085700.1570886238; _dy_c_exps=; _dycnst=dg; _dyid=7809242661650866999; _dy_c_att_exps=; _dyus_9876164=137%7C0%7C0%7C36%7C0%7C0.0.1552327481697.1552338012943.10531.0%7C69%7C11%7C2%7C119%7C6%7C0%7C0%7C0%7C0%7C0%7C0%7C6%7C0%7C0%7C0%7C0%7C0%7C6%7C0%7C0%7C0%7C0%7C0; _dycst=dk.m.c.ws.; _dy_geo=DZ.AF.DZ_.DZ__; _dy_df_geo=Algeria..; _dy_toffset=-2; _dy_ses_load_seq=41430%3A1552940220005; _dy_soct=1000907.1001003.1552940220; sidesStatus=%22%7B%5C%22currentLeft%5C%22%3Atrue%2C%5C%22currentRight%5C%22%3Atrue%2C%5C%22none%5C%22%3Afalse%2C%5C%22mono%5C%22%3Afalse%2C%5C%22twice%5C%22%3Atrue%2C%5C%22lastOpenedSide%5C%22%3A%5C%22left%5C%22%7D%22; _gid=GA1.2.535853043.1554748763; _gat=1",
-      "leshopch" => "eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6ImRjOTAwZjkwLTc5ZmItNDdlZC05YmQwLWFhODM0ZmZmNzhlZiJ9..R17TPTF2rv3VWMqC.sAMLU0gHBvU2phY3vJ-Ezu9Jd9liUhzJeG4dKZT4nMz2lAn8SDGSvyhkgNW5Tv8ABy3ba2HKxay7QywvxcGo0dNTf8TBzy_Prr_pA6tRt1qJ16ro0uX24vF9tZ4CrLn3-_RemnW_zTu9Ri5n-y7IAAd31ycMAObG4V6TVNrrIuK0ZjLyroO_9nVVM4t9SW374qWq1KjsAqJk9_hXNjaQk3wG4UQ8yAxhPsKGdgfLZf_05jWDiAKs3Fd_CwkXW4RrASeXIeJBmJWG_yqbaFW112-zkucn.4_j4qlkyPb_1h494vY4NHg"
+      "cookie" => " __cfduid=df8c785565c236372fda70bc82c67745a1549792060; _ga=GA1.2.1232152974.1549792084; _fbp=fb.1.1549792085700.1570886238; _dy_c_exps=; _dycnst=dg; _dyid=7809242661650866999; _dy_c_att_exps=; _dyus_9876164=137%7C0%7C0%7C36%7C0%7C0.0.1552327481697.1552338012943.10531.0%7C69%7C11%7C2%7C119%7C6%7C0%7C0%7C0%7C0%7C0%7C0%7C6%7C0%7C0%7C0%7C0%7C0%7C6%7C0%7C0%7C0%7C0%7C0; _dycst=dk.m.c.ws.; _dy_geo=DZ.AF.DZ_.DZ__; _dy_df_geo=Algeria..; _dy_toffset=-2; _dy_ses_load_seq=41430%3A1552940220005; _dy_soct=1000907.1001003.1552940220; sidesStatus=%22%7B%5C%22currentLeft%5C%22%3Atrue%2C%5C%22currentRight%5C%22%3Atrue%2C%5C%22none%5C%22%3Afalse%2C%5C%22mono%5C%22%3Afalse%2C%5C%22twice%5C%22%3Atrue%2C%5C%22lastOpenedSide%5C%22%3A%5C%22left%5C%22%7D%22; _gid=GA1.2.1379008846.1554917644; _gcl_au=1.1.198820196.1554917644",
+      "leshopch" => "eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6ImE4M2U5YWJmLTlhODItNDZmOC05ZWU4LTNlNjc1N2QxZjM4YiJ9..8mMhMgdA5-vFRflI.teKq1-lZNWxrrBzRHVaJ73lP3DmhA1TwUQG_aH6V7pycmQDJwmlcep-PnpOPRJNxr69hQgfzBNJ6uWHXA6USxFRpZZHRS-lQ4PP0afN291XnwyktDYgI0QCIxHp0yFR4o75-ae8H3ZacXpbQl_-oVpyoeMhZcUWFUleAWCVsigp4D5XKUhYb3pKA-FVtRVRmoGdAHUvyAYAjst4zPtnjpgZsBS0IPe7szJ8gRgh4ogAMryF7c7hOMS0MfypPsdueucFkS5ne_E4DS9Gdo-24Ivq4TiuX.afLio02QkiUKyRGNpdVzFw"
+
   }
 
 end
