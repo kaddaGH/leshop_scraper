6e350952fc24191ff6d42f49be8aa2b6dd25ab17
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-04-10 21:07:37 +0100

    Update scraper.

--- lib/headers.rb
+++ lib/headers.rb
@@ -2,12 +2,12 @@ module ReqHeaders
   SEARCH_PAGE_HEADER_REQ = {
 
       "Host" => "www.leshop.ch",
-      "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+      "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:65.0) Gecko/20100101 Firefox/65.0",
       "Accept" => "application/json, text/plain, */*",
       "Accept-Language" => "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
       "Accept-Encoding" => "gzip, deflate, br",
-      "Referer" => "https://www.leshop.ch/de/search?query=Energy%20Drinks",
-      "DNT" =>  "1",
+      "Referer" => "https://www.leshop.ch/de/search?query=Energy%20Drink",
+      "DNT" => "1",
       "Connection" => "keep-alive",
       "cookie" =>"__cfduid=df8c785565c236372fda70bc82c67745a1549792060; _ga=GA1.2.1232152974.1549792084; _fbp=fb.1.1549792085700.1570886238; _dy_c_exps=; _dycnst=dg; _dyid=7809242661650866999; _dy_c_att_exps=; _dyus_9876164=137%7C0%7C0%7C36%7C0%7C0.0.1552327481697.1552338012943.10531.0%7C69%7C11%7C2%7C119%7C6%7C0%7C0%7C0%7C0%7C0%7C0%7C6%7C0%7C0%7C0%7C0%7C0%7C6%7C0%7C0%7C0%7C0%7C0; _dycst=dk.m.c.ws.; _dy_geo=DZ.AF.DZ_.DZ__; _dy_df_geo=Algeria..; _dy_toffset=-2; _dy_ses_load_seq=41430%3A1552940220005; _dy_soct=1000907.1001003.1552940220; sidesStatus=%22%7B%5C%22currentLeft%5C%22%3Atrue%2C%5C%22currentRight%5C%22%3Atrue%2C%5C%22none%5C%22%3Afalse%2C%5C%22mono%5C%22%3Afalse%2C%5C%22twice%5C%22%3Atrue%2C%5C%22lastOpenedSide%5C%22%3A%5C%22left%5C%22%7D%22; _gid=GA1.2.1379008846.1554917644; _gcl_au=1.1.198820196.1554917644",
 
