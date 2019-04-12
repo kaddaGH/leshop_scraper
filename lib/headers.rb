4bd16b4a752b0ccf34cc460bcf7ffb417119a677
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-04-12 16:02:37 +0100

    Update header.

--- lib/headers.rb
+++ lib/headers.rb
@@ -4,12 +4,13 @@ module ReqHeaders
       "accept" => "application/json, text/plain, */*",
       "accept-encoding" => "gzip, deflate, br",
       "accept-language" => "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5",
-      "cookie" => "__cfduid=df8c785565c236372fda70bc82c67745a1549792060; _ga=GA1.2.1232152974.1549792084; _fbp=fb.1.1549792085700.1570886238; _dy_c_exps=; _dycnst=dg; _dyid=7809242661650866999; _dy_c_att_exps=; _dycst=dk.m.c.ws.; _dy_geo=DZ.AF.DZ_.DZ__; _dy_df_geo=Algeria..; _dy_toffset=-2; _dy_ses_load_seq=41430%3A1552940220005; _dy_soct=1000907.1001003.1552940220; sidesStatus=%22%7B%5C%22currentLeft%5C%22%3Atrue%2C%5C%22currentRight%5C%22%3Atrue%2C%5C%22none%5C%22%3Afalse%2C%5C%22mono%5C%22%3Afalse%2C%5C%22twice%5C%22%3Atrue%2C%5C%22lastOpenedSide%5C%22%3A%5C%22left%5C%22%7D%22; _gid=GA1.2.1379008846.1554917644; _gcl_au=1.1.198820196.1554917644; _gat=1",
-      "leshopch" => "eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6ImE4M2U5YWJmLTlhODItNDZmOC05ZWU4LTNlNjc1N2QxZjM4YiJ9..ndmw6O-VKMGoEIcu.9K8BE2ohC3rT2rvp4J0cem4NndUgxSqmiBV3ymxNj3v7FPUzuuSJb4nvAodXZIoXGDPBq-1owtl-IL0aRJ480jiENF852q6UA2MrJauXobZM76sZuMrsIrzIJXdiHYNxu-NspCPSt-TJaTbKYuRp-Eqp5Vlc4nhaLoaEf_T7sBlcgle0VCE4LJxtOkBduPoWtDKMUIv787kPgWYb6-yCNJA0fqvM1lkSntraA81ghu4WPbOUxp9IfO1GwWkSmqCoCm1RO_WAgb9hirbK7B6x1jwI9rHe.dbqf25YPRdxFRCccy3i3zw",
+      "cookie" => " __cfduid=df8c785565c236372fda70bc82c67745a1549792060; _ga=GA1.2.1232152974.1549792084; _fbp=fb.1.1549792085700.1570886238; _dy_c_exps=; _dycnst=dg; _dyid=7809242661650866999; _dy_c_att_exps=; _dycst=dk.m.c.ws.; _dy_geo=DZ.AF.DZ_.DZ__; _dy_df_geo=Algeria..; _dy_toffset=-2; _dy_ses_load_seq=41430%3A1552940220005; _dy_soct=1000907.1001003.1552940220; sidesStatus=%22%7B%5C%22currentLeft%5C%22%3Atrue%2C%5C%22currentRight%5C%22%3Atrue%2C%5C%22none%5C%22%3Afalse%2C%5C%22mono%5C%22%3Afalse%2C%5C%22twice%5C%22%3Atrue%2C%5C%22lastOpenedSide%5C%22%3A%5C%22left%5C%22%7D%22; _gcl_au=1.1.198820196.1554917644; _gid=GA1.2.310825355.1555079078; _gat=1",
+      "leshopch" => "eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6ImE4M2U5YWJmLTlhODItNDZmOC05ZWU4LTNlNjc1N2QxZjM4YiJ9..R917fjtOn_qmpoFV.TMhpzGEzCTQKb9pHykP1YDg5EflH7PQorzb5jLvblH901V3A3DARUYkuq_zfoXV4qhnNQ3bAucC_vwt_5elbKtSvRaaxWMx8zPNOEn7HOE0horUIiKMzvCe7XTm7PEtkZwzdtF3nXc9lqfj-tptfF9g9pnwt82FFzB2avvReyQqDuy6eY0MXkW5iUVLEg2hRtUKJglT-Hbd_pzzwZ8kCwutQZoYEDEIeBLnItBcsUwEkfsRFW31_h4jh109qaukxoW-HmOy8uT0zDdRsSvqk-MTL2ndg.FLKFSgSMT88ZybXoB4J3IQ",
       "referer" => "https://www.leshop.ch/de/search?query=Energy%20Drink",
       "user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
 
 
   }
+
 end
 
