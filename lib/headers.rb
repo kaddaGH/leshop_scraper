da37edce76139fa2cfc9e0cf296563f1cb84ad5f
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-04-17 00:56:09 +0100

    Update header.

--- lib/headers.rb
+++ lib/headers.rb
@@ -5,7 +5,8 @@ module ReqHeaders
       "accept-encoding" => "gzip, deflate, br",
       "accept-language" => "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5",
       "cookie" => "__cfduid=df8c785565c236372fda70bc82c67745a1549792060; _ga=GA1.2.1232152974.1549792084; _fbp=fb.1.1549792085700.1570886238; _dy_c_exps=; _dycnst=dg; _dyid=7809242661650866999; _dy_c_att_exps=; _dycst=dk.m.c.ws.; _dy_geo=DZ.AF.DZ_.DZ__; _dy_df_geo=Algeria..; _dy_toffset=-2; _dy_ses_load_seq=41430%3A1552940220005; _dy_soct=1000907.1001003.1552940220; _gcl_au=1.1.198820196.1554917644; sidesStatus=%22%7B%5C%22currentLeft%5C%22%3Atrue%2C%5C%22currentRight%5C%22%3Atrue%2C%5C%22none%5C%22%3Afalse%2C%5C%22mono%5C%22%3Afalse%2C%5C%22twice%5C%22%3Atrue%2C%5C%22lastOpenedSide%5C%22%3A%5C%22left%5C%22%7D%22; _gid=GA1.2.1974316685.1555452143; _gat=1",
-      "leshopch" => "eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6IjM1YzljZTY0LTY5Y2MtNDZjMC05ZmY1LTlhNzU2YzA2MDUwMSJ9..HCTO97fcrOS5oQp6.gtJr4DDAWornFf2e_awLzKQTx3YYGTe3CGJEFx5JEH-Wn7npgxtgtSpdBHLu8yDoOlP2xG9O-JbwgZMvkZa1l8iGKnosI-4I_22TqEUOsEABhpo5Sjk1dpPQpCLr9eeZdl2ZR2mkrzTmrHlqXzP26K7iMeeMmS7daqndC7MUeJUAeg--2kMeU-r6v0eW0sIOfif_vYe1-_wdMQ9In_BJ03xuRvwE8zGd8xL84Oqr3qH59tngnRGMoGb-lME0cbf-45Ufr0Z1ANZR7qnqN8GEuzA4aXxw.61oJd26cYjvDT9QJvB4gEQ",
+      "leshopch" => "eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6IjM1YzljZTY0LTY5Y2MtNDZjMC05ZmY1LTlhNzU2YzA2MDUwMSJ9..1k6QKOcAk9h6HBRw.mYPtERH99iGNBxc-dpOLAME3cg6EX71xIAsSeIZ_U7LT-SwfexNG9ViS6LWj76v_D9MG9LQXlYdtsak6F709vTUsvlEQMZlZzznT8JFxzpUqIX2j0wkSMeARXptYSncAcOY2nztwBzNBf6QWwp345H1qnd8-VetvNGONnm1snsCEJ-Zt-XC5jPHnOUj4ZQUZJ4LKl_dDu3atbQx1JuWoMuXADlNyNnQlQFFg-BFpwaSqFVj1ny7VMRW-CtzY0HM47Dktf6QCLaaSzJbLGwxxo67DRy0h.-PxLkgBoixD0GBLi2aLFng",
+
       "referer" => "https://www.leshop.ch/de/search?query=Energy%20Drink",
       "user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
 
