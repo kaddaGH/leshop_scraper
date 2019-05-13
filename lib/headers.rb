09eaa15734fc9844b5b90e7581c3ca2f7f2c2870
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-05-13 18:03:01 +0100

    Update scraper.

--- lib/headers.rb
+++ lib/headers.rb
@@ -4,9 +4,9 @@ module ReqHeaders
       "accept" => "application/json, text/plain, */*",
       "accept-encoding" => "gzip, deflate, br",
       "accept-language" => "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5",
-      "cookie" => "__cfduid=df8c785565c236372fda70bc82c67745a1549792060; _ga=GA1.2.1232152974.1549792084; _fbp=fb.1.1549792085700.1570886238; _dy_soct=1000907.1001003.1552940220; _gcl_au=1.1.198820196.1554917644; _gid=GA1.2.1171200509.1555615253; sidesStatus=%22%7B%5C%22currentLeft%5C%22%3Atrue%2C%5C%22currentRight%5C%22%3Atrue%2C%5C%22none%5C%22%3Afalse%2C%5C%22mono%5C%22%3Afalse%2C%5C%22twice%5C%22%3Atrue%2C%5C%22lastOpenedSide%5C%22%3A%5C%22left%5C%22%7D%22; _gat=1",
+      "cookie" => "__cfduid=df8c785565c236372fda70bc82c67745a1549792060; _ga=GA1.2.1232152974.1549792084; _fbp=fb.1.1549792085700.1570886238; _dy_soct=1000907.1001003.1552940220; _gcl_au=1.1.198820196.1554917644; sidesStatus=%22%7B%5C%22currentLeft%5C%22%3Atrue%2C%5C%22currentRight%5C%22%3Atrue%2C%5C%22none%5C%22%3Afalse%2C%5C%22mono%5C%22%3Afalse%2C%5C%22twice%5C%22%3Atrue%2C%5C%22lastOpenedSide%5C%22%3A%5C%22left%5C%22%7D%22; _gid=GA1.2.1836287314.1557766808; _gat=1",
+      "leshopch" => "eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6IjM1Njc0MTRmLTRiZWItNDExNi1hZWU4LTgzMGZhMWM4ZDM1NiJ9..UTZB2lwTiv067P94.ZDTgtivvMnOtT-n0bCIXS7FLZnrXKC5KPghlX7BMCX53hDycgvUBe6UNSS5kgW1FziVzBUzIWO7v4R30fbZDXbpZ0R5zgPn8pbpriZnztIazD8lqZpewUGkl2-uqGeKrc_9ZInN7xWdj0IZW8C1ccT0FX8_U10qvBbnSgja9jRmtoxz75GXb5j4QE0NkDiORNK_T6HREqJY-kbaFP4iDC4bxYLgyaAkHYRwnaNS6t6YKv2uG65dwqWB0knbGfxyb6nmmKSRKK36oWsgH2wi6h9m6q_Kr.7baW_18ut5ZFot8t112WQw",
 
-      "leshopch" => "eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6ImFiODFjNzg2LWViYWYtNDU0Yy1hZTZmLTZhM2JhZTNlMzhhMCJ9..IJp0R1YvWjppcciB.VQm72XiqBEKq6x6Czy18PdK-c958UhxzfE-Cyg3TokH05fgkf8eoXlJk8XaWGuOeLMWQKZnPCGPZB3dSPWaaAmrCZGojFqcrS75W-bIuhDjTdReLV24-nhLTLuiZqbuFfriuyzT75xjXMpHvnZJ47yh62fAGz-Za6eF7T9lBpru6Y-qNfchjG1a6Xht7XDiZnUG7DI__9DCLSGeWj7B9Hh7Xp_G7bbyB-e8sKolhTTWL9SAPwYPlaAsN9amFJNOtRrOg248mXMFg-ovEfgMsUvZmGYve.Dd9Z-vRocnFlDVGKS9f-lQ",
       "referer" => "https://www.leshop.ch/de/search?query=Energy%20Drink",
       "user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
 
